6640a2eebf54789a776b2ac1485d491c39e1faac
alexandresuehiro <107832355+alexandresuehiro@users.noreply.github.com>
2024-07-18 19:53:48 +0000

    reflexoes

--- pages/reflexoes.py
+++ pages/reflexoes.py
@@ -13,7 +13,12 @@ with st.container():
 
     st.markdown(
         """
-        Nesta introdução, são descritos alguns tópicos importantes para o entendimento do projeto, dentre eles oque é o petróleo Brent, a EIA e o IPEA.
+        Após a análise dos resultados, considerando o histórico de mudanças no valor do Petróleo, podemos afirmar que alguns eventos futuros poderão ter impacto no seu valor:
+        1) Eleição nos EUA: devido ao impacto de uma eventual mudança na Casa Branca, o mundo poderá reagir e o preço do petróleo mudar.
+        2) Fim da guerra da Ucrânia: caso ocorra um final de conflito, o mercado pode reagir melhor e suspendar as sanções contra a Rússia, o que pode impactar até mesmo a nossa balança comercial.
+        3) Preço de commodities: se a produção de commodities acelerar, a demanda por petróleo aumentará, o que beneficia os países produtores, como a Rússia e os países do Oriente Médio.
+                - Impacto direto: muitos países da Europa, cuja matriz energética depende do petróleo, sofrerão impactos nos gastos com importações e isso pode causar atritos políticos com muitas consequências.
+                - Impacto direto: isso pode beneficiar os países que já produzem commodities e aumentar a produção das mesmas, o que pode causar excesso e derrubar os preços das mesmas.
     """
     )
 
